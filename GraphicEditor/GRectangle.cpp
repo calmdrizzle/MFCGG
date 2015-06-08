4a136211b23dcb01611689f96b5d3129d1dd5355
SeoGB <rmszh1@naver.com>
2015-06-08 22:24:31 +0900

    rectangle 형

--- GraphicEditor/GRectangle.cpp
+++ GraphicEditor/GRectangle.cpp
@@ -10,12 +10,47 @@ IMPLEMENT_SERIAL(GRectangle, Object, 0)
 
 GRectangle::GRectangle()
 {
-
 }
 
 GRectangle::~GRectangle()
 {
 }
 
+void GRectangle::draw(CDC* pDc) {
 
+	CPen pen(PS_SOLID, m_Thickness, m_lineColor);
+
+	CPen *oldPen = pDc->SelectObject(&pen);
+	CGdiObject* oldBrush = pDc->SelectStockObject(NULL_BRUSH);
+
+	pDc->Rectangle(m_topLeft.x, m_topLeft.y, m_bottomRight.x, m_bottomRight.y);
+
+	pDc->SelectObject(oldBrush);
+	pDc->SelectObject(oldPen);
+}
+
+int GRectangle::drawType(void) {
+	return RECTANGLE;
+}
+
+void GRectangle::Serialize(CArchive& ar) {
+	//int type = m_type;
+	ar << drawType();
+	ar << m_topLeft << m_bottomRight/* << m_type*/ << m_Thickness << m_lineColor << m_fillColor;
+}
+
+
+
+void GRectangle::setRect(CPoint& topLeft, CPoint& bottomRight)
+{
+	m_topLeft = topLeft;
+	m_bottomRight = bottomRight;
+}
+
+void GRectangle::setProps(int& lineWidth, COLORREF& lineColor, COLORREF& fillColor) {
+	//m_type = type;
+	m_Thickness = lineWidth;
+	m_lineColor = lineColor;
+	m_fillColor = fillColor;
+}
 // GRectangle ¸â¹ö ÇÔ¼ö
--- GraphicEditor/GRectangle.h
+++ GraphicEditor/GRectangle.h
@@ -8,9 +8,21 @@ class GRectangle : public Object
 protected:
 	DECLARE_SERIAL(GRectangle);
 
+private:
+	CPoint m_topLeft, m_bottomRight;
+	int m_Thickness;
+	COLORREF m_lineColor;
+	COLORREF m_fillColor;
+
 public:
 	GRectangle();
 	virtual ~GRectangle();
+	void draw(CDC* pDc);
+	int drawType(void);
+	virtual void Serialize(CArchive& ar);
+
+	void setRect(CPoint& topLeft, CPoint& bottomRight);
+	void setProps(int& lineWidth, COLORREF& lineColor, COLORREF& fillColor);
 };
 
 
