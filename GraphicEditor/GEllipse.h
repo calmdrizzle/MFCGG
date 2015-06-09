441ef4fd84a3327076ee7a92da49c0b61e21cb48
SeoGB <rmszh1@naver.com>
2015-06-09 22:58:59 +0900

    Ellipse 수

--- GraphicEditor/GEllipse.h
+++ GraphicEditor/GEllipse.h
@@ -6,25 +6,26 @@ class GEllipse : public Object
 {
 protected:
 	DECLARE_SERIAL(GEllipse);
-public:
-	GEllipse();
-	virtual ~GEllipse();
-	virtual void Serialize(CArchive& ar);	//직렬화
-
-	CRect virtual getBoundary();				// 리전을 반환
-	void virtual drawBoundary(CDC* cdc);
-	bool virtual isin(CPoint p);									// 리전을 반환
-	int GetPenStyle();										//펜 스타일 반환
-	COLORREF GetFillColor(void);							//채우기색 반환
-	void Move(int dX, int dY);								// 좌표 이동
-	void SetPenStyle(int penStyle);							// 펜 스타일 설정
-	void SetFillColor(COLORREF brushColor);
-	void Draw(CDC* cdc);
-
 private:
-	int m_PenStyle;							// 펜 스타일
-	COLORREF m_FillColor;					// 브러쉬 칼라
-	int m_n_HatchStyle;	
+	CPoint m_topLeft;
+	CPoint m_bottomRight;
+
+	FIGURETYPE m_type;
+	int m_Thickness;
+	COLORREF m_lineColor;
+	COLORREF m_fillColor;
+
+public:
+	GEllipse(void);
+	virtual ~GEllipse(void);
+	void draw(CDC* pDc);
+	int drawType(void);
+	void serialize(CArchive& ar);
+	void deserialize(CArchive& ar);
+
+	void setRect(CPoint& topLeft, CPoint& bottomRight);
+	void setCircle(CPoint& center, int radius);
+	void setProps(FIGURETYPE type, int& lineWidth, COLORREF& lineColor, COLORREF& fillColor);
 
 };
 
